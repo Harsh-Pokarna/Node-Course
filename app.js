dd1629c505c7dae16353728ac9070edf93320d2e
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-04-01 11:21:30 -0700

    testing vsc

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hello')
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import chalk from 'chalk'
+
+
+yargs(hideBin(process.argv)).command({
+    command: 'add',
+    describe: 'Add a note',
+    handler: function() {
+        console.log('Adding a note..')
+    }
+}).parse()
+
+yargs(hideBin(process.argv)).command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Remvoe a note')
+    }
+})
+
+console.log(yargs(process.argv.slice(2)).argv)
